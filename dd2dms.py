cf27d6b119b0b81f823112a799e56646d8a20a78
Leandro França <geoleandro.franca@gmail.com>
2020-06-23 11:32:32 -0300

    updates

--- dd2dms.py
+++ dd2dms.py
@@ -8,19 +8,19 @@ def dd2dms(dd, n_digits, feature, parent):
     Transform decimal degrees to degrees, minutes and seconds.
     <h2>Example usage:</h2>
     <ul>
-      <li>dd2dms(dd, 3) -> -12°12'34.741''</li>
+      <li>dd2dms(dd, 3) -> -12dd12mm34.741ss</li>
     </ul>
     """
     if dd != 0:
         graus = int(floor(abs(dd)))
         resto = round(abs(dd) - graus, 8)
         if dd < 0:
-            texto = '-' + str(graus) + '°'
+            texto = '-' + str(graus) + 'dd'
         else:
-            texto = str(graus) + '°'
+            texto = str(graus) + 'dd'
         minutos = int(floor(60*resto))
         resto = round(resto*60 - minutos, 10)
-        texto = texto + '{:02d}'.format(minutos) + '\''
+        texto = texto + '{:02d}'.format(minutos) + "mm"
         segundos = resto*60        
         if round(segundos,n_digits) == 60:
             minutos += 1
@@ -29,9 +29,9 @@ def dd2dms(dd, n_digits, feature, parent):
             graus += 1
             minutos = 0
         if n_digits < 1:
-            texto = texto + '{:02d}'.format(int(segundos)) + "\""
+            texto = texto + '{:02d}'.format(int(segundos)) + 'ss'
         else:
-            texto = texto + ('{:0' + str(3+n_digits) + '.' + str(n_digits) + 'f}').format(segundos) + "\""
+            texto = texto + ('{:0' + str(3+n_digits) + '.' + str(n_digits) + 'f}').format(segundos) + 'ss'
         return texto
     else:
-        return '0°00\'' + ('{:0' + str(3+n_digits) + '.' + str(n_digits) + 'f}').format(0)
+        return "0dd00mm" + ('{:0' + str(3+n_digits) + '.' + str(n_digits) + 'f}').format(0) + 'ss'
